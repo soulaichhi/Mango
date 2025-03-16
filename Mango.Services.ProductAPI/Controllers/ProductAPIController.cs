f574b87ea1318506f7f88222e64171af8f914356
soulaichhi <chhibou.soulaimane@gmail.com>
2025-03-16 23:15:03 +0000

    Create Product API microservice

---
+++ Mango.Services.ProductAPI/Controllers/ProductAPIController.cs
@@ -0,0 +1,36 @@
+﻿using AutoMapper;
+using Mango.Services.ProductAPI.Data;
+using Mango.Services.ProductAPI.Models;
+using Mango.Services.ProductAPI.Models.Dto;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Mango.Services.ProductAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductAPIController :ControllerBase
+    {
+        private readonly AppDbContext _db;
+        private ResponseDto _response;
+        private IMapper _mapper;
+        public ProductAPIController(AppDbContext db, IMapper mapper) {
+            _db = db;
+            _mapper = mapper;
+            _response = new ResponseDto();
+        }
+        [HttpGet]
+        [Authorize]
+        public ResponseDto Get()
+        {
+            try {
+                IEnumerable<Product> objList = _db.Products.ToList();
+                _response.Result = _mapper.Map<IEnumerable<ProductDto>>(objList);
+            }catch(Exception ex) {
+                _response.IsSuccess = false;
+                _response.Message = ex.Message;
+            }
+            return _response;
+        }
+    }
+}
